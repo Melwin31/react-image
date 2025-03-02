4c91f5d16922436545d2c5243ea8f775d96aab5a
Melwin31 <melwin272005@gmail.com>
2025-03-02 20:32:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import Navbar from "./components/navbar"
+import Images from "./components/Images"
+import Next from "./components/Next"
+
+function App(){
+
+return(<div>
+  <Navbar></Navbar>
+  <Images></Images>
+  <Next></Next>
+</div>)
+}
+export default App
